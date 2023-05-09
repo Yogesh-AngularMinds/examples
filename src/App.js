66423f941170725a9908e707b6efd09615858668
Yogesh-AngularMinds <yogesh.r@angularminds.in>
2023-05-09 12:01:57 +0530

    Usememo added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 //useRef
 import React from "react";
 import UseRef from "./useRef/UseRef";
+import UseMemo from "./useMemo/UseMemo";
 
 const App = () => {
   return (
     <div>
-      React Examples UseRef
-      <UseRef />
+      {/* React Examples UseRef
+      <UseRef /> */}
+
+      {/* useMemo*/}
+      <UseMemo />
     </div>
   );
 };
